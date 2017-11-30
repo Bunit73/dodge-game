7a0aa290ecdfbcb2b26b8e638d9e7d726ae7a23a
BUnit73 <ford2bm@gmail.com>
2017-11-30 09:33:17 -0700

    Start in the center and move around

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Player from "./components/Player";
+import Score from "./components/Score";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Score/>
         <Player/>
       </View>
     );
@@ -17,7 +19,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
