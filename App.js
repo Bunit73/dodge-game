dc5c8a367ac57dea87e4bb0632c4a6ddc7ae32fa
BUnit73 <ford2bm@gmail.com>
2017-11-30 08:05:27 -0700

    Score updates every second

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Score from "./components/Score";
 
 export default class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {score: 0};
+    }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <Score s={this.state.score}/>
       </View>
     );
   }
@@ -17,7 +21,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
